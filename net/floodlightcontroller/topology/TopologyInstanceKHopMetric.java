3932fd98e3172db8255bcab6c94d40f174bd017e
LucaPrete <preteluca@gmail.com>
2013-07-24 11:05:48 +0200

    License and copyright added

--- net/floodlightcontroller/topology/TopologyInstanceKHopMetric.java
+++ net/floodlightcontroller/topology/TopologyInstanceKHopMetric.java
@@ -1,3 +1,34 @@
+/**
+ * Copyright (C) 2013 Luca Prete, Simone Visconti, Andrea Biancini, Fabio Farina - www.garr.it - Consortium GARR
+ * 
+ * This is an extended, modified version of the original TopologyInstance
+ * file provided with Floodlight 0.90
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * 
+ * Implementation of the Floodlight TopologyInstance for KHopMetric service.
+ * A representation of a network topology.  Used internally by 
+ * {@link TopologyManager}
+ * 
+ * @author Luca Prete <luca.prete@garr.it>
+ * @author Andrea Biancini <andrea.biancini@garr.it>
+ * @author Fabio Farina <fabio.farina@garr.it>
+ * 
+ * @version 0.90
+ * @see net.floodlightcontroller.topology.TopologyManagerKHopMetric
+ * 
+ */
+
 package net.floodlightcontroller.topology;
 
 import java.util.ArrayList;
@@ -21,10 +52,6 @@ import edu.asu.emit.qyan.alg.model.Path;
 import edu.asu.emit.qyan.alg.model.Vertex;
 import edu.asu.emit.qyan.alg.model.abstracts.BaseVertex;
 
-/**
- * A representation of a network topology.  Used internally by 
- * {@link TopologyManager}
- */
 @LogMessageCategory("Network Topology")
 public class TopologyInstanceKHopMetric extends TopologyInstance{
 
